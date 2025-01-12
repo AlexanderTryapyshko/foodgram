20671c30753275853296ad02c341e9a92e4c7723
Alexander Tryapyshko <stoolik@gmail.com>
2025-01-12 17:47:18 +0300

    after second review

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 """Urls.py."""
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+from django.views.generic import TemplateView
 
 from api.views import (
     IngredientsViewSet,
@@ -19,4 +20,8 @@ router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('doc/',
+         TemplateView.as_view(template_name='redoc.html'),
+        name='doc'
+    ),
 ]
